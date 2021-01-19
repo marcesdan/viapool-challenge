a5d208f38963c32360a9116b853f8f881117573e
marces <marianod93@gmail.com>
2021-01-19 10:03:51 -0300

    pruebo config vars en heroku

--- src/config/DebugConfig.js
+++ src/config/DebugConfig.js
@@ -5,6 +5,7 @@ const useMirage = process.env.NODE_ENV === 'development';
 
 Sentry.init({ dsn: process.env.SENTRY_DSN });
 Sentry.captureMessage('Hola mundo');
+alert(process.env.SENTRY_DSN);
 
 const mirageLoad = async () => useMirage && import(
   /* webpackChunkName: "mirage-config" */
